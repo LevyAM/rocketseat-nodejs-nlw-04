8cc6d2c3d896aa9ab8cbd10baf9e7776e1f6f347
LevyAM <levyzudo@msn.com>
2021-02-23 20:35:07 -0300

    trabalhando com migrations e organizando a API

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,12 @@
-import express, { response } from 'express'
+import 'reflect-metadata';
+import express, { response } from 'express';
+import './database';
+import { router } from './routes';
+
 
 const app = express();
 
-//GET => BUSCA
-//POST => SALVAR
-//PUT => ALTERAR
-//DELETE => DELETAR
-//PATCH => ALTERAÇÃO ESPECÍFICA
-
-//http://localhost:3333/
-app.get("/", (req, res) => {
-    return res.json({message: "Hello World - NLW#04"});
-})
-
-
-//1 param => Rota(Recurso API)
-//2 param => request, response
-app.post("/", (req, res) => {
-    //Recebeu os dados para salvar
-    return res.json({message: "Os dados foram salvos com sucesso"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, ()=> console.log("Server is running!!"));
