33b047b96f9643e2c5396efbfd0414f757494313
LevyAM <levyzudo@msn.com>
2021-02-24 21:49:02 -0300

    Inclusão de testes automatizados com testes de integração

---
+++ src/repositories/SurveyRepository.ts
@@ -0,0 +1,15 @@
+
+
+import { request } from 'express';
+import { EntityRepository, Repository } from 'typeorm';
+import { Survey } from '../models/Survey';
+import { User } from '../models/User';
+
+@EntityRepository(Survey)
+class SurveyRepository extends Repository <Survey>{
+    
+
+
+}
+
+export {SurveyRepository}
